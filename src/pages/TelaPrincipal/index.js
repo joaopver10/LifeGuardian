f800ac8e017f3da2a708035c1e65ec7f0c8a89fc
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-10-27 19:23:30 -0300

    mapa com geolocalização

--- src/pages/TelaPrincipal/index.js
+++ src/pages/TelaPrincipal/index.js
@@ -1,15 +1,46 @@
-import React, { useContext } from 'react'
-import { AutenticaContext } from '../../context/autentica'
+import React, { useState, useEffect } from 'react'
+import MapView from 'react-native-maps'
+
+import { Background, TextNome } from './styles'
 import Header from '../../components/Header'
-import {Background, TextNome} from './styles'
 
 export default function TelaPrincipal() {
-    const { user, deslogando } = useContext(AutenticaContext)
+    const [mapa, setMapa] = useState(null)
+
+    useEffect(() => {
+        async function pegaLocal() {
+            await navigator.geolocation.getCurrentPosition(
+                async ({ coords: { latitude, longitude } }) => {
+                    setMapa({
+                        latitude: latitude,
+                        longitude: longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    })
+                },
+                {
+                    timeout: 2000,
+                    maximumAge: 1000,
+                }
+
+            )
+        }
+        pegaLocal()
+    }, [])
 
     return (
         <Background>
             <Header />
-            <TextNome>{user && user.nome}</TextNome>
+            <TextNome>Sua Localização em tempo real</TextNome>
+
+            <MapView
+                style={{ width: 360, height: 400, marginTop: 10 }}
+                region={mapa}
+                showsUserLocation
+                loadingEnabled
+                minZoomLevel={18}
+                rotateEnabled={true}
+            />
         </Background>
     )
 }
--- src/pages/TelaPrincipal/styles.js
+++ src/pages/TelaPrincipal/styles.js
@@ -9,6 +9,6 @@ background-color: #0CB7F5;
 export const TextNome= styled.Text`
 font-size: 20px;
 font-weight: bold;
-margin-bottom: 10px;
 align-self: center;
+margin-top: 35;
 `
