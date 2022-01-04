e40e13d31beeb4a4a4e150672a2104c0c51bb962
João Pedro <55984774+joaopver10@users.noreply.github.com>
2022-01-03 22:24:12 -0300

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import 'react-native-gesture-handler'
-import React, { useState, useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'react-native'
-
-import AuthProvider from './src/context/autentica'
-import Rotas from './src/routes'
-
-console.disableYellowBox = true
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AuthProvider>
-        <StatusBar backgroundColor='#131313' barStyle='light-content' />
-        <Rotas />
-      </AuthProvider>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
